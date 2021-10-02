f3f8a41653a2f7ceba293ce579071f081216aee1
anggidast <adastariana@gmail.com>
2021-10-02 20:30:53 +0700

    feat: mobile product detail

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-dialog v-model="card">
+  <q-dialog v-model="card" class="desktop-only">
     <q-card class="my-card" flat square bordered style="width: 700px; max-width: 80vw;">
       <q-card-section horizontal>
         <q-carousel class="col-6" animated v-model="slide" arrows infinite control-color="grey-7" navigation swipeable>
@@ -32,6 +32,44 @@
 
         <div class="q-ma-xs absolute-right">
           <q-btn icon="close" size="lg" flat round dense v-close-popup @click="closeModal" />
+        </div>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+
+  <q-dialog v-model="card" class="mobile-only" full-width full-height>
+    <q-card class="my-card" flat square bordered>
+      <q-card-section>
+        <q-carousel animated v-model="slide" arrows infinite control-color="grey-7" navigation swipeable>
+          <q-carousel-slide v-for="(url, i) in image_urls" :key="i" :name="i + 1">
+            <q-img :src="product[`image_url${i + 1}`]" class="full-height" />
+          </q-carousel-slide>
+        </q-carousel>
+
+        <q-card-section>
+          <div class="text-h4 text-grey-9 text-weight-regular poppins-font">
+            {{ product.name }}
+          </div>
+          <div class="q-ml-xs q-mt-sm q-mb-md text-subtitle1 text-weight-bold text-grey">Rp. {{ product.price.toLocaleString('id-ID') }}</div>
+          <div class="q-ml-xs text-body1 text-weight-medium text-grey-7">Description:</div>
+          <div class="q-ml-xs text-body2 text-grey-7">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates doloremque commodi non! Vel, asperiores eos, aliquam repellendus iste
+            optio voluptatem autem, commodi reprehenderit cum modi iusto excepturi et enim illum?
+          </div>
+          <div class="q-ml-xs q-mt-lg q-mb-xs text-caption text-grey-7">Stock: {{ product.stock }}</div>
+          <q-btn
+            @click="addToCart(product.id, $route.path)"
+            v-close-popup
+            class="full-width"
+            color="black"
+            size="md"
+            label="add to cart"
+            icon-right="o_shopping_bag"
+          />
+        </q-card-section>
+
+        <div class="q-ma-xs absolute-right">
+          <q-btn icon="close" size="md" flat round dense v-close-popup @click="closeModal" />
         </div>
       </q-card-section>
     </q-card>
