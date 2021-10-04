c788b951bffa35249318637a26ad9ea6e6eafc6b
anggidast <adastariana@gmail.com>
2021-10-04 21:57:49 +0700

    add: address page

---
+++ src/views/Address.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="q-pa-md">
+    <q-table title="My Address" :rows="rows" :columns="columns" row-key="name" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddressPage',
+  data() {
+    return {
+      columns: [
+        { name: 'name', label: 'Name', fields: 'name' },
+        { name: 'receiver', label: 'Receiver', fields: 'receiver' },
+        { name: 'address', label: 'Address', fields: 'address' },
+        { name: 'city', label: 'City', fields: 'city' },
+        { name: 'province', label: 'Province', fields: 'province' },
+      ],
+      rows: [
+        {
+          name: 'Home',
+          receiver: 'Anggi Dastariana',
+          address: 'Perumahan Taman Kutabumi',
+          city: 'Tangerang',
+          province: 'Banten',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
